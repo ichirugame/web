39f52910902333601e2737257a99ef63c35ee7ad
ichirugame <takara9102008@gmail.com>
2023-01-07 21:11:44 +0900

    テスト

---
+++ ip/index.php
@@ -0,0 +1,26 @@
+<?php
+$IP = $_SERVER['REMOTE_ADDR'];
+$IPH = gethostbyaddr($IP);
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IPアドレス</title>
+</head>
+<body>
+    <p>自分のIPアドレス:
+        <?php
+        echo $IP;
+        ?>
+    </p>
+    <p>
+        自分のホストアドレス:
+        <?php
+        echo $IPH;
+        ?>
+    </p>
+</body>
+</html>
