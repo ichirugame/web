ed217862e96cb6d8134d60c5f804de5638ea78a0
ichirugame <takara9102008@gmail.com>
2023-01-07 16:00:58 +0900

    追加

---
+++ sql/index.php
@@ -0,0 +1,45 @@
+<?php
+$db = $_POST['sql'];
+$reset = null && $_POST['reset'];
+if($reset){
+    $db = null;
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sql文</title>
+    <link rel="stylesheet" type="text/css" href="../css/sql.css">
+</head>
+<body>
+    <p>sql文</p>
+    <form action="./index.php" method="POST">
+    <textarea name="sql" style="width:500px; height:100px;"></textarea>
+        <br>
+        <input type="submit" value="送信">
+    </form>
+    <form action="./index.php" method="POST">
+        <input type="submit" type="reset" name="reset" value="リセットする">
+    </form>
+    <p>結果</p>
+    <?php
+    if($db){
+    ?>
+    <p>phpに直書き可能かもしれません</p>
+    <p>
+        $query = "
+        <?php
+        echo $db;
+        ?>
+        ";
+    </p>
+    <p>$mysqli = new mysqli(localhost, user_name, password, db_name);</p>
+    <p>$mysqli->query($query);</p>
+    <?php
+    }
+    ?>
+</body>
+</html>
