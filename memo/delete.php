ba1c6ba503e2c570c66d42d0df0dd75225a40f5c
ichirugame <takara9102008@gmail.com>
2023-01-11 20:25:46 +0900

    追加

--- memo/delete.php
+++ memo/delete.php
@@ -3,6 +3,7 @@ if(isset($_COOKIE['memo'])){
     setcookie("memo", null, time() - 30);
     echo "削除できました。";
 ?>
+<meta name="viewport" content="width=device-width,initial-scale=1">
 <link rel="stylesheet" type="text/css" href="../css/style.css">
 <script>
 setTimeout("redirect()", 1750);
@@ -14,6 +15,7 @@ function redirect(){
 }elseif(empty($_COOKIE['memo'])){
     echo "削除されています。";
 ?>
+<meta name="viewport" content="width=device-width,initial-scale=1">
 <link rel="stylesheet" type="text/css" href="../css/style.css">
 <script>
 setTimeout("redirect()", 1750);
